31dfad1eb05d843f8633133b452ce6cc1a27a164
Hafizan090804 <hafizsecondhero@gmail.com>
2025-11-25 18:42:15 +0800

    update

--- backend/server.js
+++ backend/server.js
@@ -11,5 +11,5 @@ app.use(express.json());
 
 app.use("/api/visitors", visitorsRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5300;
 app.listen(PORT, () => console.log("Server running on port", PORT));
